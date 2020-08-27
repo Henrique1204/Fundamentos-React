6c51a2463800c0369a1865b540f35d1a968ec10b
Paulo <pauloh16.pdr@gmail.com>
2020-08-27 08:05:19 -0300

    Adicionar componente com arrow function

---
+++ fundamentos-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Primeiro from "./componentes/basicos/Primeiro";
+import ComParametros from "./componentes/basicos/ComParametros";
+import Fragmento from "./componentes/basicos/Fragmento"
+
+// Funcção reduzida, só vai funcionar com o export default
+// pq os outros export precisam do nome para serem referenciados na hora do import
+// O parânteses serve para indicar a expressão, porém funciona sem tbm
+export default () => (
+    <div id="app">
+        <h1>Fundamentos do React</h1>
+        <Fragmento />
+        <ComParametros titulo="Situação do Aluno" aluno="Paulo" nota={8} />
+        <Primeiro></Primeiro>
+    </div>
+);
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Primeiro from "./componentes/basicos/Primeiro";
-import ComParametros from "./componentes/basicos/ComParametros";
-import Fragmento from "./componentes/basicos/Fragmento"
+import App from "./App";
 
 const elemento = document.getElementById("root");
 
-ReactDOM.render(
-    <div id="app">
-        <Primeiro></Primeiro>
-        <ComParametros titulo="Situação do Aluno" aluno="Paulo" nota={8} />
-        <Fragmento />
-    </div>,
-    elemento
-);
+ReactDOM.render(<App/>, elemento);
